ce9be8cce5a6542e7ab4aea50715e1f50aaa587f
Saikumar <sai@devopsguru.in>
2022-01-29 16:45:57 +0000

    my first commit of the source code

---
+++ file1.java
@@ -0,0 +1,9 @@
+total 3248
+-rw-rw-r-- 1 saikumarjayanti saikumarjayanti       5 Jan 29 16:30 check_idle
+drwxr-xr-x 2 jenkins         jenkins            4096 Jan 29 15:29 hsperfdata_jenkins
+drwxr-xr-x 2 root            root               4096 Jan 29 15:30 hsperfdata_root
+drwx------ 2 jenkins         jenkins            4096 Jan 29 15:29 jetty-0_0_0_0-8080-war-_-any-1500847068863941794
+drwx------ 2 saikumarjayanti saikumarjayanti    4096 Jan 29 15:31 ssh-cm02h4AVQ4KY
+drwx------ 3 root            root               4096 Jan 29 15:29 systemd-private-c9f7feac9c1e4f8aaee35b01e5b9f692-rtkit-daemon.service-rJF6Nd
+drwx------ 3 root            root               4096 Jan 29 15:29 systemd-private-c9f7feac9c1e4f8aaee35b01e5b9f692-systemd-timesyncd.service-LBG2aM
+-rw-r--r-- 1 jenkins         jenkins         3293397 Jan 29 15:29 winstone5102903429149105117.jar
---
+++ file2.java
@@ -0,0 +1,8 @@
+check_idle
+hsperfdata_jenkins
+hsperfdata_root
+jetty-0_0_0_0-8080-war-_-any-1500847068863941794
+ssh-cm02h4AVQ4KY
+systemd-private-c9f7feac9c1e4f8aaee35b01e5b9f692-rtkit-daemon.service-rJF6Nd
+systemd-private-c9f7feac9c1e4f8aaee35b01e5b9f692-systemd-timesyncd.service-LBG2aM
+winstone5102903429149105117.jar
